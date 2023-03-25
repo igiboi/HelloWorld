34a2493ee253bc6d8afb99f98348890d73e2fa9b
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2023-03-25 14:44:27 -0400

    learning about method overloading

---
+++ src/MinutesToSeconds.java
@@ -0,0 +1,28 @@
+public class MinutesToSeconds {
+    public static void main(String[] args) {
+
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-65, 45));
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "Invalid input: time values cannot be negative.";
+        }
+
+        int minutes = seconds / 60;
+        return getDurationString(minutes, seconds);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0 || seconds < 0) {
+            return "Invalid input: time values cannot be negative.";
+        }
+        int hours = minutes / 60;
+
+        int remainingMinutes = minutes % 60;
+        int remainingSeconds = seconds % 60;
+
+        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+    }
+}
